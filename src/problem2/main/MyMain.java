5b9bf58dc4245a8431771906223ebc1f4c1c4667
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-28 19:15:15 +0530

    import MyBinarySearchTree of problem1 and add the elements in the tree

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -7,7 +7,20 @@
 package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
 public class MyMain {
 
+    public static void main(String[] args) {
 
-}
+MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+
+        myBinarySearchTree.add(12);
+        myBinarySearchTree.add(45);
+        myBinarySearchTree.add(76);
+        myBinarySearchTree.add(32);
+        myBinarySearchTree.add(20);
+
+        }
+    }
+
