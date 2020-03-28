a6da0afe69b123f42491765721c4c645293286f9
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-28 17:15:59 +0530

    add method implemented in MyBinarySearchTree class

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,8 +5,14 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
-public class MyBinarySearchTree {
+public class MyBinarySearchTree<E> implements MyBinarySearchTreeADT<E> {
 
-
+    @Override
+    public boolean add(E data) {
+        return false;
+    }
 }
--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -6,5 +6,5 @@
  */
 package problem1.node;
 
-public class TreeNode {
+public class TreeNode<E> {
 }
