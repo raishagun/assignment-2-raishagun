78e9057968cbc89a8b1f694b1913e3b7e6fb4dd5
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-28 17:18:22 +0530

    declare and initialize the private feilds

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -11,6 +11,11 @@ import problem1.node.TreeNode;
 // to implement BinarySearchTree
 public class MyBinarySearchTree<E> implements MyBinarySearchTreeADT<E> {
 
+
+    private TreeNode<E> root;
+    public int numberOfLeftChild = 0;
+    private int maxLevel = 0;
+
     @Override
     public boolean add(E data) {
         return false;
