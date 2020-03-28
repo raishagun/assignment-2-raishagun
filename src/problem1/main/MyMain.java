c21718dc2c57a12cb1ad623e23dab0046c9fd846
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-28 18:42:14 +0530

    add the elements by creating the object of MyBinarySearchTree class

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,8 +7,26 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
 
+    public static void main(String[] args) {
 
-    
+            MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<Integer>();
+            myBinarySearchTree.add(11);
+            myBinarySearchTree.add(44);
+            myBinarySearchTree.add(42);
+            myBinarySearchTree.add(78);
+            myBinarySearchTree.add(13);
+            myBinarySearchTree.add(74);
+            myBinarySearchTree.add(150);
+            myBinarySearchTree.add(180);
+            myBinarySearchTree.add(25);
+            myBinarySearchTree.add(90);
+            myBinarySearchTree.print();
+            System.out.println("number of nodes who don't have their left child " + myBinarySearchTree.numberOfLeftChild);
+        }
+
+
 }
