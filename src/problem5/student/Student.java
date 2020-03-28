b0aef41fd8598dcf97470dc162052dd2ca65b499
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-28 20:42:03 +0530

    constructor for the class Student

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -9,4 +9,12 @@ package problem5.student;
 public class Student {
     private int rollNumber;
     private String name;
+    public Student(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public Student(int rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+    }
 }
