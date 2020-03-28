b2e4e18088684041e1f81bfa293bd92233b5fd34
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-28 19:46:17 +0530

    constructor for Node class

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -10,6 +10,15 @@ public class Node<E> {
     private E data;
     private Node<E> next;
 
+    public Node(E data) {
+        this.data = data;
+    }
+
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
+
     public E getData() {
         return data;
     }
