aacba078c7e0d432676dc1ec3a580a205d585e5f
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-28 21:00:22 +0530

    create object of MyPriorityQueue class

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,10 +5,20 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem5.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
     public static void main(String[] args) {
-
+            MyPriorityQueue<Student> myPriorityQueue = new MyPriorityQueue<>();
+            myPriorityQueue.add(new Student(55, "shagun"));
+            myPriorityQueue.add(new Student(5, "apooorva"));
+            myPriorityQueue.add(new Student(37, "rishi"));
+            myPriorityQueue.add(new Student(28, "palak"));
+            System.out.println(myPriorityQueue);
+        }
     }
-}
+
