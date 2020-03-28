3c17affe812d5b97f76f779f297db73c6bb3d69a
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-28 18:16:27 +0530

    addRecursive method to recursively the data

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -16,8 +16,23 @@ public class MyBinarySearchTree<E> implements MyBinarySearchTreeADT<E> {
     public int numberOfLeftChild = 0;
     private int maxLevel = 0;
 
+    public TreeNode<E> getRoot() {
+        return root;
+    }
+
+
     @Override
     public boolean add(E data) {
-        return false;
+        root = addRecursive(root,data);
+        return true;
     }
+     private TreeNode<E> addRecursive(TreeNode<E> currentNode,E data){
+
+
+
+
+        return currentNode;
+    }
+
+
 }
