dea151e856257ba960b5a58267dcf4cce31d990d
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-31 19:42:34 +0530

    enter the details of the student

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,7 +6,9 @@
  */
 package problem5.main;
 
+import problem5.Student5;
 import problem5.circularqueue.MyCircularQueue;
+import problem5.node.Node;
 
 import java.util.Scanner;
 
@@ -19,6 +21,28 @@ public class MyMain {
         System.out.println("Enter number of records you wants to enter");
         int numberOfRecords = scan.nextInt();
 
+        while (numberOfRecords != 0) {
+            Student5 student = new Student5();
+            Node node = new Node();
+
+            System.out.println("Enter name");
+            student.setName(scan.next().trim());
+            System.out.println("Enter roll number");
+            student.setRollNumber(scan.nextInt());
+            System.out.println("Enter backlogs");
+            int backLogs = scan.nextInt();
+            if (backLogs > 2) {
+                System.out.println("This student is not allowed");
+                continue;
+            }
+            student.setBackLogCounter(backLogs);
+
+            node.setStudent(student);
+
+            circularQueue.enQueue(node);
+
+            numberOfRecords--;
+        }
     }
 
 }
