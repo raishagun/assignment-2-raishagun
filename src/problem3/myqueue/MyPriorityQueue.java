874aa269eee5200b85e6b90e8b5fd8ce80e48298
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-31 14:09:28 +0530

    create adt interface

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -11,73 +11,20 @@ import problem3.node.Node;
 
 public class MyPriorityQueue<E> implements adt<E> {
 
-    private int size = 0;
-    private Node<E> front;
-    private Node<E> rear;
-
-    private Node<E> getNode(int index) {
-        Node<E> response = front;
-        if (index < 0 && index > size) {
-            throw new IndexOutOfBoundsException(Integer.toString(index));
-        } else {
-            for (int i = 1; i < index && front != null; i++) {
-                response = response.getNext();
-            }
-        }
-        return response;
-    }
-    private void Addd(E data) {
-        Node<E> node = new Node<>(data);
-        for (int i = 0; i < size; i++) {
-            Node<E> node1 = getNode(size - i);
-            if (node.getData().equals(node1.getData())) {
-                node.setNext(node1.getNext());
-                node1.setNext(node);
-            } else {
-                continue;
-            }
-        }
-        rear = getNode(size);
-        front = getNode(1);
-        size++;
-    }
-
 
     @Override
     public void add(E data) {
-        if (size == 0) {
-            Node<E> node = new Node<E>(data, null);
-            front = node;
-            rear = node;
-            size++;
-        } else {
-            Addd(data);
-        }
+
     }
 
     @Override
     public E remove() {
-        E data = front.getData();
-        front = front.getNext();
-        size--;
-        return data;    }
+        return null;
+    }
 
     @Override
     public E peek() {
-        E data = front.getData();
-        return data;
-    }
-
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("[");
-        Node<E> temp = front;
-        for (int i = 0; i < size && temp != null; i++) {
-            E data = temp.getData();
-            sb.append(data);
-            sb.append((i < size - 1) ? ",\n" : "");
-            temp = temp.getNext();
-        }
-        sb.append("]");
-        return sb.toString();
+        return null;
     }
 }
+
