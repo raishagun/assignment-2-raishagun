8939e3a4b1b98ee2d2a42040f9b55d1a1130ed23
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-31 18:05:10 +0530

    input the numbers added in bst

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,10 +5,23 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+import problem1.node.TreeNode;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter elements which are added in binary search tree");
+        String s = sc.nextLine();
+        String[] ar = s.split(" ");
     }
 }
+
+
