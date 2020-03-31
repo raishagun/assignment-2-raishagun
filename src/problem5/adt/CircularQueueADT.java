560f3cb8d750d68c7fb68e3d6baafa3317e268d8
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-31 18:28:16 +0530

    create abstract methods

--- src/problem5/adt/CircularQueueADT.java
+++ src/problem5/adt/CircularQueueADT.java
@@ -1,5 +1,17 @@
 package problem5.adt;
 
 public interface CircularQueueADT<E> {
+    public void enqueue(E data);
 
+    //a method to remove element from front of queue
+    public E dequeue();
+
+    //a method to get front element of queue
+    public E peek();
+
+    // a method to check whether queue is empty
+    public boolean isEmpty();
+
+    // to get size of queue
+    public int getSize();
 }
