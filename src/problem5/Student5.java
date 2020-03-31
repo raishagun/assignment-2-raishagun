e04151fd3b0ce34b859009514796bd929e42ffdd
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-31 19:01:33 +0530

    create private fields and constructors and getter and setters for the class

--- src/problem5/Student5.java
+++ src/problem5/Student5.java
@@ -1,5 +1,40 @@
 package problem5;
 
 public class Student5 {
+    private String name;
+    private int rollNumber;
+    private int backLogCounter;
 
+    public int getBackLogCounter() {
+        return backLogCounter;
+    }
+
+    public void setBackLogCounter(int backLogCounter) {
+        this.backLogCounter = backLogCounter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "---------------------------------------" + "\n" +
+                "Name -- " + getName() + "\n" +
+                "Roll Number -- " + getRollNumber() + "\n" +
+                "Back Log Value -- " + getBackLogCounter() + "\n" +
+                "---------------------------------------";
+    }
 }
