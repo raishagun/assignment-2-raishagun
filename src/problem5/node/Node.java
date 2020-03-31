9a66a580929a4631a7b29bd68075ed76ffd16f3e
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-31 19:05:03 +0530

    create the private fields and constructors and getters and setters of the Node class

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -8,7 +8,26 @@ package problem5.node;
 
 //import problem5.student.Student5;
 
+import problem5.Student5;
+
 // to define node properties
 public class Node {
+    private Student5 student;
+    private Node next;
 
+    public Student5 getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student5 student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
