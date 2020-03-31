306e85051db9ca88f5f434410bca4fb6ce566280
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-31 19:36:00 +0530

    create object of MyCircularQueue and add the number of entries you want

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,12 +6,18 @@
  */
 package problem5.main;
 
+import problem5.circularqueue.MyCircularQueue;
+
 import java.util.Scanner;
 
 //executable class
 public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        MyCircularQueue circularQueue = new MyCircularQueue();
+
+        System.out.println("Enter number of records you wants to enter");
+        int numberOfRecords = scan.nextInt();
 
     }
 
