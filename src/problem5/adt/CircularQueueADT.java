2423260cbc545fb5ef91da4e0c41966957269fd6
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-31 18:25:59 +0530

    create adt

---
+++ src/problem5/adt/CircularQueueADT.java
@@ -0,0 +1,5 @@
+package problem5.adt;
+
+public interface CircularQueueADT<E> {
+
+}
--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -7,4 +7,5 @@
 package problem5.circularqueue;
 //to implement circular queue
 public class MyCircularQueue {
+
 }
