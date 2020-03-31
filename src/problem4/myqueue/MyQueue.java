3d6980ce54403c64a0336681d8dc7eafa39dd73e
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-31 17:05:29 +0530

    getters and setters

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -57,6 +57,21 @@ public class MyQueue<E> implements MyQueueADT<E> {
             this.next = next;
         }
 
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
     }
     }
 
