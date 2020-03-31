670e62cd4d4beed11978c24be0eb0eb6d86c65bf
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-31 19:59:25 +0530

    remove() method create

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -55,8 +55,10 @@ public class MyPriorityQueue<E> implements adt<E> {
 
     @Override
     public E remove() {
-        return null;
-    }
+        E data = front.getData();
+        front = front.getNext();
+        size--;
+        return data;    }
 
     @Override
     public E peek() {
