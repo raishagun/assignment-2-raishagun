d67691e583f933c939724c458320909c931f37f2
raishagun <shagun.gla_cs18@gla.ac.in>
2020-03-31 17:34:44 +0530

    toString() method create

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -59,6 +59,22 @@ public class MyQueue<E> implements MyQueueADT<E> {
     public boolean isEmpty() {
         return size == 0;
     }
+
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("[");
+        Node<E> temp = front;
+        for (int i = 0; i < size && temp != null; i++) {
+            E data = temp.getData();
+            sb.append(data);
+            sb.append((i < size - 1) ? "," : "");
+            temp = temp.getNext();
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+
+
     private static class Node<E> {
         E data;
         Node<E> next;
